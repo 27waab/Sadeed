29a2a238171911b8b172d0ac6eb673541b640efd
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-04-05 02:18:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-let loginBtn = document.querySelector(".login"),
-overlay = document.querySelector(".ovarlay"),
-card = document.querySelector(".login-card");
+let loginBtn = document.querySelector(".login");
+let overlay = document.querySelector(".ovarlay");
+let card = document.querySelector(".login-card");
+let nameInput = document.getElementById("name");
+let submitButton = card.querySelector(".content a button");
 
 loginBtn.onclick = () => {
     overlay.classList.add("show");
     card.classList.add("show");
 };
+
 overlay.onclick = () => {
     overlay.classList.remove("show");
     card.classList.remove("show");
-}
+};
+
+submitButton.onclick = (event) => {
+    event.preventDefault();
+    const username = nameInput.value.trim();
+    if (username) {
+        localStorage.setItem("username", username);
+        window.location.href = "home.html";
+        nameInput.value = "";
+    } else {
+        alert("الرجاء إدخال اسم المستخدم.");
+    }
+};
